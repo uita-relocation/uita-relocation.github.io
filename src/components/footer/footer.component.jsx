ae1b812618bec8d0453a64a6b0593055e3fa03e4
amarkevych <aw.markevich@gmail.com>
2022-03-27 16:00:33 +0300

    Add initial footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,7 +4,9 @@ import "./footer.styles.scss"
 
 function Footer() {
     return (
-        <footer className="footer"/>
+        <footer className="footer">
+            <p className="copyright">Copyright © 2022. All rights reserved</p>
+        </footer>
     );
 }
 
