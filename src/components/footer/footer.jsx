47093aa0b329a6e6e7747e873b66a85154dd4825
amarkevych <aw.markevich@gmail.com>
2022-04-01 10:26:55 +0300

    Remove comments

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,18 +1,3 @@
-// import React from "react";
-//
-// import "./footer.styles.scss"
-//
-// function Footer() {
-//     return (
-//         <footer className="footer">
-//             <p className="copyright">Copyright © 2022. All rights reserved</p>
-//         </footer>
-//     );
-// }
-//
-// export default Footer;
-
-
 import React from "react";
 import {Box, Container, Grid, Link, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
