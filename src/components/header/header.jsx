1fe3026fbdc6a5e51fe4303b5e11c709418786f3
amarkevych <aw.markevich@gmail.com>
2022-04-04 17:43:18 +0300

    Fix nav method

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -171,7 +171,7 @@ export function Header() {
                                     label={label}
                                     id={`tabs-${index}`}
                                     aria-controls={`tabs-${index}`}
-                                    onClick={() => navigate(to)}
+                                    onClick={() => setActive(label)}
                                 />
                             )
                         })}
